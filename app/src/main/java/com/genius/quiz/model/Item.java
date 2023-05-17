f2304876ab941b9b747d14b1bb90bd8f67c0f8dd
Mohammed Awadh <mo3@techhunters.techhunters.com>
2023-05-18 02:24:08 +0300

    first commit

---
+++ app/src/main/java/com/genius/quiz/model/Item.java
@@ -0,0 +1,14 @@
+package com.genius.quiz.model;
+
+public class Item {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
